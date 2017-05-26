7e0de952ac030ce972eef23d3e327482fcf51cda
zactus <zpecenak@eng.ucsd.edu>
2017-05-26 08:27:25 -0700

    updating getYbus code

--- main/getYbus.m
+++ main/getYbus.m
@@ -9,18 +9,27 @@ function [YbusOrderVect, YbusPhaseVect, Ycomb, Ybus, buslist, dssCircuit]=getYbu
 % [c] = FeederReduction(Bus_upstream,c);
 %Check both inputs are met
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% get Ybus from OpenDSS for feeder.
 fprintf('\nGetting YBUS and buslist from OpenDSS: ')
 tic
+if isfield(circuit,'pvsystem')
+	circuit=rmfield(circuit,'pvsystem');
+end
+if isfield(circuit,'load')
+	circuit=rmfield(circuit,'load');
+end
+if isfield(circuit,'regcontrol')
+	circuit=rmfield(circuit,'regcontrol');
+end
 
 %load the circuit and generate the YBUS
 p = WriteDSS(circuit,'test',0,pwd); o = actxserver('OpendssEngine.dss');
-	
+
 dssText = o.Text; dssText.Command = 'Clear'; cDir = pwd;
 dssText.Command = ['Compile "' p '"']; dssCircuit = o.ActiveCircuit;
-	dssText.Command = 'Set controlmode = off';
+dssText.Command = 'Set controlmode = off';
 dssSolution = dssCircuit.Solution;
 dssSolution.MaxControlIterations=100;
 dssSolution.MaxIterations=100;
@@ -28,11 +37,11 @@ dssSolution.InitSnap; % Initialize Snapshot solution
 dssSolution.dblHour = 0.0;
 dssSolution.Solve;
 %Convert the Ybus to a matrix
-	
+
 Ybus=dssCircuit.SystemY;
 ineven=2:2:length(Ybus); inodd=1:2:length(Ybus);
 Ybus=Ybus(inodd)+1i*Ybus(ineven); Ybus=reshape(Ybus,sqrt(length(Ybus)),sqrt(length(Ybus)));
-	
+
 [YbusOrderVect, YbusPhaseVect]=strtok(dssCircuit.YNodeOrder,'\.');
 YbusPhaseVect=str2num(cell2mat(strrep(YbusPhaseVect,'.','')));
 Ycomb=dssCircuit.YNodeOrder;
